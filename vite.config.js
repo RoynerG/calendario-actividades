714e7c26d14cd038e97c7fd6870f21d3ebc419dc
RoynerG <roynergh@hotmail.com>
2025-05-16 16:04:04 -0500

    FIX: deployment

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // → Redirige el AdapterDateFnsV3 al AdapterDateFns estándar
-      "@mui/x-date-pickers/AdapterDateFnsV3":
-        "@mui/x-date-pickers/AdapterDateFns",
-    },
-  },
 });
